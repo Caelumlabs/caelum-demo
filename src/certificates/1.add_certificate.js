2c08c3087dfdf336db086713b11ed695db725a5b
imagobea <himagobea@gmail.com>
2021-10-21 09:13:38 +0200

    Updated README plus claim_admin, add_cert scripts

--- src/certificates/1.add_certificate.js
+++ src/certificates/1.add_certificate.js
@@ -1,48 +1,47 @@
-const fs = require('fs')
 const path = require('path')
-const faker = require('faker')
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
+
+const rootPath = path.join(__dirname, "../../");
+require("dotenv").config({ path: path.join(rootPath, ".env") });
 
 const Caelum = require('caelum-sdk');
-const caelum = new Caelum(process.env.SUBSTRATE);
-const adminInfo = require('../json/admin.json');
+const caelum = new Caelum(process.env.SUBSTRATE, process.env.NETWORK);
+const adminInfo = require(path.join(rootPath, "data", "admin.json"));
 
-// Main function.
-const setup = async (did) => {
-  // Load User and Idspace.
+const addCertificate = async (did) => {
+  // Validate that needed data is available
+  if (did == null || did === "") {
+    console.error("Missing variables or their values in .env");
+    process.exit();
+  }
+
+  // Load User and Idspace
   const {user, idspace} = await caelum.connect(adminInfo, did);
 
-  let tagForm = {
-    title: 'Certificat de reciclatge',
-    description: 'Certificat de Reciclatge per part del ciutadà',
-    url: 'https://valls.cat',
-    logo: 'https://valls.cat',
-    requirements: 'Reciclar',
-    issuedTo: 'Persona'
+  // Certificate example
+  const tagForm = {
+    title: 'Spacewalk certificate', // string
+    description: 'The astronaut got out of hte shuttle while in space', // string | null
+    url: 'https://valls.cat', // string | null
+    logo: null, // string | null
+    requirements: null, // string | null
+    issuedTo: 'Persona'  // string['Nominal', 'Organisation', 'Persona'] - 'Organisation' is default
   }
-  let api = await idspace.sdk.call('tag', 'add', {data: tagForm})
-  console.log(api)
-/*
-  tagForm.title = 'Certificat de compostatge'
-  tagForm.description = 'Certificat de Compostatge per part del ciutadà',
-  api = await idspace.sdk.call('tag', 'add', {data: tagForm})
 
-  tagForm.title = 'Certificat de vsita a la deixalleria'
-  tagForm.description = 'Certificat de Visita a la deixalleria i us dels seus serveis',
-  api = await idspace.sdk.call('tag', 'add', {data: tagForm})
+  // Create the certificate via SDK
+  const { certificateDid, certificateId } = await idspace.sdk.call('tag', 'add', { data: tagForm });
+  console.log("certificateId:", certificateId);
+  console.log("certificateDid:", certificateDid);
 
-  api = await idspace.sdk.call('api', 'getAll')
-  console.log(' Certificates: ', api.length)
-  */
-  process.exit()
+  process.exit();
 }
 
-/**
-* Main
-**/
 const main = async () => {
-  await setup(process.env.DID)
+  try {
+    await addCertificate(process.env.DID);
+  } catch (error) {
+    console.log("Something went wrong!", error);
+    process.exit();
+  }
 }
-main()
 
-
+main();
--- src/users/1.claim_admin.js
+++ src/users/1.claim_admin.js
@@ -1,37 +1,48 @@
-const fs = require('fs')
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
+const fs = require('fs');
+const path = require('path');
+
+const rootPath = path.join(__dirname, "../../");
+require("dotenv").config({ path: path.join(rootPath, ".env") });
 
 const Caelum = require('caelum-sdk');
-const caelum = new Caelum(process.env.SUBSTRATE);
+const caelum = new Caelum(process.env.SUBSTRATE, process.env.NETWORK);
 
-// Main function.
 const claimAdmin = async (did, secretCode) => {
+  // Validate that needed data is available
+  if (did == null || did === "" ||
+    secretCode == null || secretCode === "") {
+    console.error("Missing variables or their values in .env");
+    process.exit();
+  }
+
   await caelum.connect();
-  // Creates a new user Object with new connection keys.
-  const user = await caelum.newUser()
 
-  // Opens a new session with the Idspace.
-  const idspace = await caelum.getOrganizationFromDid(did);
-  const session = await idspace.getSession('admin');
+  // Creates a new User object with new connection keys
+  const user = await caelum.newUser();
 
-  // This is the QR code we can see in the Web.
-  console.log('QR Code : ' + session.connectionString)
+  // Opens a new session with the Idspace
+  const organisation = await caelum.getOrganizationFromDid(did);
+  const session = await organisation.getSession('admin');
+  console.log("QR Code:" + session.connectionString);
 
-  // Register the user with the secret Code.
-  await user.registerConnectionString(session.connectionString, secretCode)
+  // Complete the registration process (with the secretCode received via email)
+  const peerDid = await user.registerConnectionString(session.connectionString, secretCode);
+  console.log("Registered peerDid (public key):", peerDid);
 
-  // Export user to JSON.
-  const userJson = await user.export()
-  await fs.writeFileSync( path.resolve(__dirname, '../json') + '/admin.json', userJson, 'utf8')
+  // Export user to JSON (on the phone, the app will encrypt the file)
+  const userJson = await user.export();
+  fs.writeFileSync(path.join(rootPath, "data", "admin.json"), userJson, "utf8");
 
-  process.exit()
+  process.exit();
 }
 
-/**
-* Main
-**/
 const main = async () => {
-  await claimAdmin(process.env.DID, process.env.SECRET)
+  try {
+    await claimAdmin(process.env.DID, process.env.SECRET);
+  } catch (error) {
+    console.log("Something went wrong!", error);
+    process.exit();
+  }
 }
-main()
+
+main();
