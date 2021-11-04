78dc9c7c351827ac224b79d406f0f2e0e3f47d75
imagobea <himagobea@gmail.com>
2021-11-04 18:59:37 +0100

    Added upload logic, but needs fixing

--- src/workflows/call_wf5_dipuLleida.js
+++ src/workflows/call_wf5_dipuLleida.js
@@ -1,8 +1,11 @@
 const faker = require("faker");
+const FormData = require("form-data");
+const fs = require("fs");
 const path = require("path");
 
 const rootPath = path.join(__dirname, "../../");
 require("dotenv").config({ path: path.join(rootPath, ".env") });
+const filePath = path.join(rootPath, "data", "test.jpg");
 
 const Caelum = require("caelum-sdk");
 const caelum = new Caelum(process.env.SUBSTRATE, process.env.NETWORK);
@@ -42,9 +45,28 @@ const callWorkflow = async (did, workflowId, apiToken) => {
   };
 
   // Start workflow via SDK
-  const response = await idspace.sdk.call("workflow", "set", { data: workflowForm });
-  if (response === false) return;  // SDK intercepted bad response
-  console.log(response);
+  const response1 = await idspace.sdk.call("workflow", "set", { data: workflowForm });
+  if (response1 === false) return;  // SDK intercepted bad response
+  console.log(response1);
+
+  // Upload image
+  const { stateId } = response1;
+  const imageData = fs.readFileSync(filePath);
+  const formData = new FormData();
+  formData.append("file", imageData, { filePath, contentType: "image/jpg" });
+  formData.append(
+    "workflow",
+    JSON.stringify({
+      stateId,
+      actionId: 2,
+      partyId: 1,
+      apiToken,
+    }),
+  );
+  console.log(formData.file);
+  const response2 = await idspace.sdk.call("workflow", "upload", { form: formData });
+  if (response2 === false) return;
+  console.log(response2);
 
   return;
 };
