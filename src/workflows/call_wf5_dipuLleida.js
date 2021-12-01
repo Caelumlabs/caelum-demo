69259e7701d8c4cc362b1943d60da881d8bd47dc
imagobea <himagobea@gmail.com>
2021-12-01 16:43:50 +0100

    Updated Workflow5 and call

--- src/workflows/call_wf5_dipuLleida.js
+++ src/workflows/call_wf5_dipuLleida.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const faker = require("faker");
 const FormData = require("form-data");
 const fs = require("fs");
@@ -22,9 +23,42 @@ const callWorkflow = async (did, workflowId, apiToken) => {
   // Load User and Idspace (user is Dipu technician)
   const { user, idspace } = await caelum.connect(false, did);
 
-  // Data (e.g. App Loop)
-  const sessionToken = "k4jrp0j4ue5bl9gpahsh0q8u2i" // GET https://glpi95.tic.gal/apirest.php/initSession
+  /* ========
+     App LooP
+     ========
+  */ 
+  // Request TICGal session token
+  const appToken = process.env.INTEGRATION_TICGAL_APP_TOKEN;
+  const sessionRes = await axios.get(
+    "https://glpi95.tic.gal/apirest.php/initSession", {
+      auth: {
+        "username": process.env.INTEGRATION_TICGAL_USERNAME,
+        "password": process.env.INTEGRATION_TICGAL_PASSWORD
+      },
+      headers: {
+        "App-Token": appToken,
+      }
+    }
+  )
+  if (sessionRes.status !== 200) return "Bad response";
+  console.log(sessionRes.data);
+  const { session_token } = sessionRes.data;
 
+  // User info from idspace (this is the person managing devices via LooP)
+  const tecnic = {
+    givenName: faker.name.firstName(),
+    familyName: faker.name.lastName(),
+    govId: null,
+    telephone: "+34 655 66 33 21",
+    additional: {
+      "id": 2 // TICGal User.id
+    },
+    get email() {
+      return `${this.givenName.toLowerCase()}@email.com`;
+    }
+  };
+
+  // QR Scan returns device, e.g.
   const dispositiu = {
     id: 17, // TICGal id
     type: "Computer", // Not returned
@@ -32,28 +66,50 @@ const callWorkflow = async (did, workflowId, apiToken) => {
     serial: "", // serial
   };
 
+  // Search user by email, e.g.
   const usuari = {
-    givenName: faker.name.firstName(), // TICGal User.name
-    familyName: faker.name.lastName(), // User.realname
-    govId: null, // Does not exist
+    givenName: "Daniel", // TICGal User.name
+    familyName: "Couso", // User.realname
+    govId: null, // ? Don't think it exists
     telephone: "+34 677 88 55 44", // User.mobile
+    email: "daniel@ticgal.com", // User.UserEmail.email
     additional: {
       "id": 64 // User.id
     },
-    get email() {
-      return `${this.givenName.toLowerCase()}@email.com`;
-    }
   };
 
+  // POST request to TICGals /saveSignature
+  const headersConfig = {
+    "App-Token": appToken,
+    "Session-Token": session_token
+  };
+
+  const formData = new FormData();
+
   const input = {
-    _filename: ["signature.png"],
-    tech_id: 2, // To Do - will come from idspace user.additional...
+    _filename: ["signature.png"], // The name of the signature file in the Flutter app
+    tech_id: tecnic.additional.id,
     user_id: usuari.additional.id,
     type: 1, // To Do - 1 entrega, 2 devolució
     items: [`${dispositiu.type}|${dispositiu.id}`]
   };
+  formData.append("uploadManifest", JSON.stringify({input}));
 
-  // Workflow example
+  const stream = fs.createReadStream(filePath);
+  formData.append("file", stream, { filePath, contentType: "image/png" });
+  
+  Object.assign(headersConfig, formData.getHeaders());
+
+  const signatureRes = await axios.post(
+    "https://glpi95.tic.gal/plugins/blockchainid/apirest.php/saveSignature",
+    formData,
+    { headers: headersConfig }
+  );
+  if (signatureRes.status !== 200) return "Bad response";
+  console.log(signatureRes.data);
+  const { hash } = signatureRes.data;
+
+  // Start workflow via SDK
   const workflowForm = {
     workflowId,
     apiToken,
@@ -61,10 +117,12 @@ const callWorkflow = async (did, workflowId, apiToken) => {
     "actionId": 1,
     "partyId": 1,
 
-    "dispositiu_deviceId": dispositiu.id,
-    "dispositiu_deviceType": dispositiu.type,
-    "dispositiu_deviceName": dispositiu.name,
-    "dispositiu_deviceSerial": dispositiu.serial,
+    "tecnic_currentGivenName": tecnic.givenName,
+    "tecnic_currentFamilyName": tecnic.familyName,
+    "tecnic_govId": tecnic.govId,
+    "tecnic_email": tecnic.email,
+    "tecnic_telephone": tecnic.telephone,
+    "tecnic_additional": tecnic.additional,
 
     "usuari_currentGivenName": usuari.givenName,
     "usuari_currentFamilyName": usuari.familyName,
@@ -73,35 +131,17 @@ const callWorkflow = async (did, workflowId, apiToken) => {
     "usuari_telephone": usuari.telephone,
     "usuari_additional": usuari.additional,
 
-    "request_h_appToken": false, // nulls are skipped
-    "request_h_sessionToken": sessionToken,
-    "request_f_uploadManifest": JSON.stringify({input}),
-    "request_f_filename[0]": false,
+    "dispositiu_deviceId": dispositiu.id,
+    "dispositiu_deviceType": dispositiu.type,
+    "dispositiu_deviceName": dispositiu.name,
+    "dispositiu_deviceSerial": dispositiu.serial,
+
+    "PDF_hash": hash
   };
 
-  // Start workflow via SDK
-  const response1 = await idspace.sdk.call("workflow", "set", { data: workflowForm });
-  if (response1 === false) return;  // SDK intercepted bad response
-  console.log(response1);
-
-  // Upload image
-  const { stateId } = response1;
-  const formData = new FormData();
-  const stream = fs.createReadStream(filePath);
-  formData.append("file", stream, { filePath, contentType: "image/png" });
-  formData.append(
-    "workflow",
-    JSON.stringify({
-      stateId,
-      actionId: 2,
-      partyId: 1,
-      apiToken,
-    }),
-  );
-
-  const response2 = await idspace.sdk.call("workflow", "upload", { form: formData });
-  if (response2 === false) return;
-  console.log(response2);
+  const response = await idspace.sdk.call("workflow", "set", { data: workflowForm });
+  if (response === false) return; // SDK intercepted bad response
+  console.log(response);
 
   return;
 };
